29b95d688853f37a055aed4d905c73553bbcf872
as-snehagn <sganesh@autonomoustuff.com>
2020-01-27 13:58:58 -0600

    Rebase from 'upstream'

--- include/pacmod3/pacmod3_common.h
+++ include/pacmod3/pacmod3_common.h
@@ -1,17 +1,30 @@
-#ifndef PACMOD3_COMMON_H
-#define PACMOD3_COMMON_H
+// Copyright (c) 2019 AutonomouStuff, LLC
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
-/*
-* Unpublished Copyright (c) 2009-2017 AutonomouStuff, LLC, All Rights Reserved.
-*
-* This file is part of the PACMod ROS 1.0 driver which is released under the MIT license.
-* See file LICENSE included with this software or go to https://opensource.org/licenses/MIT for full license details.
-*/
+#ifndef PACMOD3_PACMOD3_COMMON_H
+#define PACMOD3_PACMOD3_COMMON_H
 
 #include <memory>
 #include <mutex>
 
-#include <pacmod3_core.h>
+#include <pacmod3/pacmod3_core.h>
 #include <ros/ros.h>
 
 #include <pacmod_msgs/SystemCmdBool.h>
@@ -25,6 +38,7 @@
 #include <pacmod_msgs/DateTimeRpt.h>
 #include <pacmod_msgs/DetectedObjectRpt.h>
 #include <pacmod_msgs/DoorRpt.h>
+#include <pacmod_msgs/EngineRpt.h>
 #include <pacmod_msgs/HeadlightAuxRpt.h>
 #include <pacmod_msgs/InteriorLightsRpt.h>
 #include <pacmod_msgs/LatLonHeadingRpt.h>
@@ -35,10 +49,6 @@
 #include <pacmod_msgs/RearLightsRpt.h>
 #include <pacmod_msgs/ShiftAuxRpt.h>
 #include <pacmod_msgs/SteerAuxRpt.h>
-#include <pacmod_msgs/SteeringPIDRpt1.h>
-#include <pacmod_msgs/SteeringPIDRpt2.h>
-#include <pacmod_msgs/SteeringPIDRpt3.h>
-#include <pacmod_msgs/SteeringPIDRpt4.h>
 #include <pacmod_msgs/SteerSystemCmd.h>
 #include <pacmod_msgs/SystemRptBool.h>
 #include <pacmod_msgs/SystemRptFloat.h>
@@ -52,4 +62,4 @@
 #include <pacmod_msgs/WiperAuxRpt.h>
 #include <pacmod_msgs/YawRateRpt.h>
 
-#endif
+#endif  // PACMOD3_PACMOD3_COMMON_H
